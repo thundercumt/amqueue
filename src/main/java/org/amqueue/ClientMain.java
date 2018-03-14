8f6c460b8977a59c4c45c264ff41968cf79dc05e
thunder <thunder.liu@synchronoss.com>
2018-03-14 19:49:37 +0800

    project initialiation

---
+++ src/main/java/org/amqueue/ClientMain.java
@@ -0,0 +1,10 @@
+package org.amqueue;
+
+public class ClientMain {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/main/java/org/amqueue/types/LString.java
@@ -0,0 +1,33 @@
+package org.amqueue.types;
+
+import java.io.UnsupportedEncodingException;
+
+public class LString {
+    private byte[] value; //utf8 encoding
+    private int offset;
+    private int len;
+
+    public LString(byte[] octets, int offset, int len) {
+        if (octets == null
+                || offset < 0 || offset >= octets.length
+                || len < 0 || len > octets.length) {
+            throw new IllegalArgumentException();
+        }
+
+        this.value = octets;
+        this.offset = offset;
+        this.len = len;
+    }
+
+    public LString(LString s) {
+        value = new byte[s.len];
+    }
+
+    public String toString() {
+        try {
+            return new String(value, offset, len, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("invalid value encoding", e);
+        }
+    }
+}
